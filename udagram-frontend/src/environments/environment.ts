6308eb9ce0d8e57b2b34ae4e0c66204f055d2580
Wassim M <wassim.mayyasi@gmail.com>
2024-10-05 15:07:16 +0300

    chore: added all deployment and services files and adjust frontend

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a3fb997bce13b4dc48e139c9a5de013e-62448443.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
